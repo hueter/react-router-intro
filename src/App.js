18b09094f0eb5f2386af19d20473f587abd9bf72
Michael Hueter <mthueter@gmail.com>
2018-07-24 10:53:25 -0700

    secret route added

--- src/App.js
+++ src/App.js
@@ -1,21 +1,72 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Link } from 'react-router-dom';
 import './App.css';
 
 const Welcome = props => {
-  return <h1>hello world</h1>;
+  return (
+    <div>
+      <h1>Welcome Home</h1>
+    </div>
+  );
 };
 
 const About = props => {
-  return <h1>What about what about what about</h1>;
+  return (
+    <div>
+      <h1>What about what about what about</h1>
+      <Link to="/">
+        <img src="https://placekitten.com/100/100" />
+      </Link>
+    </div>
+  );
+};
+
+const Contact = props => {
+  return (
+    <div>
+      <span>{JSON.stringify(props)}</span>
+      <h1>Do not contact me. I hate people</h1>
+    </div>
+  );
+};
+
+const Secret = props => {
+  return (
+    <div>
+      <img src={props.gif} alt="a gif" />
+    </div>
+  );
 };
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Route path="/" component={Welcome} />
-        <Route path="/about" component={About} />
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+        <Route exact path="/" component={Welcome} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/contact" component={Contact} />
+        <Route exact path="/contact" component={Contact} />
+        <Route
+          exact
+          path="/secret"
+          component={routeProps => (
+            <Secret
+              gif="https://media.giphy.com/media/4m4Z29t0Lq0Sc/giphy.gif"
+              {...routeProps}
+            />
+          )}
+        />
       </div>
     );
   }
